e93b167865500294caa8c234b85d58f5f53f1fb2
Aparicio <alvmorapa87@gmail.com>
2024-02-16 15:57:31 +0100

    change sourceport range

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -32,7 +32,7 @@ resource "azurerm_network_security_group" "sec" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range          = "22"
+    source_port_range          = "*"
     source_address_prefix      = "*"
     destination_port_range     = "22"
     destination_address_prefix = "*"
